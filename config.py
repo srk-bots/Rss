3912bb3f4962c20cf7a54ca59161649342c68f53
srk-bots <sakthisivam1610@gmail.com>
2025-05-16 16:50:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,22 +17,22 @@ class API:
     HASH: Telegram API hash from https://my.telegram.org
     ID = Telegram API ID from https://my.telegram.org
     """
-    HASH = os.environ.get("API_HASH", "")
-    ID = int(os.environ.get("API_ID", 0))
+    HASH = os.environ.get("API_HASH", "378062eb0a32d8d6b1bbbe97cb63a75a")
+    ID = int(os.environ.get("API_ID", "27975779"))
 
 
 class OWNER:
     """
     ID: Owner's user id, get it from @userinfobot
     """
-    ID = int(os.environ.get("OWNER", 0))
+    ID = int(os.environ.get("OWNER", "1416841137"))
 
 
 class CHANNEL:
     """
     ID: Telegram Channel ID where the bot will post automatically
     """
-    ID = int(os.environ.get("CHANNEL_ID", 0))
+    ID = int(os.environ.get("CHANNEL_ID", "-1002692320259"))
 
 
 class WEB:
